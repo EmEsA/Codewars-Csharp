5c607b570c6201ad4e8dc2b4f4d51d60068e3ac2
Michal Walawender <michal.walawender@gmail.com>
2018-02-18 10:35:29 +0100

    egyptian fractions

---
+++ Some-Egyptian-fractions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Decomp
+{
+    public static string Decompose(string nrStr, string drStr)
+    {
+        double numerator = double.Parse(nrStr);
+        double denominator = double.Parse(drStr);
+        double reminder;
+        string result = "";
+
+        if (numerator == 0) return "[]";
+
+        if (numerator > denominator)
+        {
+            if (numerator % denominator == 0) return $"[{numerator / denominator}]";
+            result += $", {Math.Floor(numerator / denominator).ToString()}";
+            numerator = numerator % denominator;
+        }
+        
+        do
+        {
+            reminder = Math.Ceiling(denominator / numerator);
+            result += $", 1/{reminder}";
+            numerator = numerator * reminder - denominator;
+            denominator = denominator * reminder;
+        } while ((numerator >= 1));
+
+        return "[" + result.Substring(2) + "]";
+    }
+}
